850e1f892895aef9ce82e1f7e78ac52ddb19b5de
Kit Langton <kit.langton@gmail.com>
2025-03-29 12:01:09 -0500

    update scanamo

--- modules/neotype-scanamo/shared/src/main/scala/neotype/interop/scanamo/Main.scala
+++ modules/neotype-scanamo/shared/src/main/scala/neotype/interop/scanamo/Main.scala
@@ -11,6 +11,14 @@ given [A, B](using nt: Newtype.WithType[A, B], df: DynamoFormat[A]): DynamoForma
 
   def write(t: B): DynamoValue = df.write(nt.unwrap(t))
 
+// Subtype
+
+given [A, B <: A](using st: Subtype.WithType[A, B], df: DynamoFormat[A]): DynamoFormat[B] = new DynamoFormat[B]:
+  def read(av: DynamoValue): Either[DynamoReadError, B] =
+    df.read(av).flatMap(a => st.make(a).left.map(err => TypeCoercionError(NeotypeValidationError(err))))
+
+  def write(t: B): DynamoValue = df.write(t)
+
 // Support types
 
 final case class NeotypeValidationError(message: String) extends Throwable:
--- modules/neotype-scanamo/shared/src/test/scala/neotype/interop/scanamo/ScanamoSpec.scala
+++ modules/neotype-scanamo/shared/src/test/scala/neotype/interop/scanamo/ScanamoSpec.scala
@@ -3,9 +3,9 @@ package neotype.interop.scanamo
 import neotype.Newtype
 import neotype.test.definitions.*
 import neotype.test.definitions.SimpleNewtype
-import zio.test.*
 import org.scanamo.*
 import org.scanamo.DynamoReadError.describe
+import zio.test.*
 
 object ScanamoSpec extends ZIOSpecDefault:
   case class Config[A](value: A)
@@ -13,31 +13,71 @@ object ScanamoSpec extends ZIOSpecDefault:
   def spec = suite("ScanamoSpec")(
     suite("NonEmptyString")(
       test("read success") {
-        val value = DynamoValue.fromString("hello")
+        val value  = DynamoValue.fromString("hello")
         val result = DynamoFormat[ValidatedNewtype].read(value)
         assertTrue(result == Right(ValidatedNewtype("hello")))
       },
       test("read failure") {
-        val value = DynamoValue.fromNumber(123)
+        val value  = DynamoValue.fromNumber(123)
         val result = DynamoFormat[ValidatedNewtype].read(value)
         assertTrue(result.left.map(describe) == Left("not of type: 'S' was 'DynNum(123)'"))
       },
       test("read validation failure") {
-        val value = DynamoValue.fromString("")
+        val value  = DynamoValue.fromString("")
         val result = DynamoFormat[ValidatedNewtype].read(value)
-        assertTrue(result.left.map(describe) == Left("could not be converted to desired type: String must not be empty"))
+        assertTrue(
+          result.left.map(describe) == Left("could not be converted to desired type: String must not be empty")
+        )
       },
       test("write success") {
-        val value = ValidatedNewtype("hello")
+        val value  = ValidatedNewtype("hello")
         val result = DynamoFormat[ValidatedNewtype].write(value)
         assertTrue(result == DynamoValue.fromString("hello"))
       }
     ),
     suite("SimpleNewtype")(
       test("read success") {
-        val value = DynamoValue.fromNumber(123)
+        val value  = DynamoValue.fromNumber(123)
         val result = DynamoFormat[SimpleNewtype].read(value)
         assertTrue(result == Right(SimpleNewtype(123)))
+      }
+    ),
+    suite("ValidatedSubtype")(
+      test("read success") {
+        val value  = DynamoValue.fromString("Hello World!")
+        val result = DynamoFormat[ValidatedSubtype].read(value)
+        assertTrue(result == Right(ValidatedSubtype("Hello World!")))
+      },
+      test("read failure") {
+        val value  = DynamoValue.fromNumber(123)
+        val result = DynamoFormat[ValidatedSubtype].read(value)
+        assertTrue(result.left.map(describe) == Left("not of type: 'S' was 'DynNum(123)'"))
+      },
+      test("read validation failure") {
+        val value  = DynamoValue.fromString("too short")
+        val result = DynamoFormat[ValidatedSubtype].read(value)
+        assertTrue(
+          result.left.map(describe) == Left(
+            "could not be converted to desired type: String must be longer than 10 characters"
+          )
+        )
+      },
+      test("write success") {
+        val value  = ValidatedSubtype("Hello World!")
+        val result = DynamoFormat[ValidatedSubtype].write(value)
+        assertTrue(result == DynamoValue.fromString("Hello World!"))
+      }
+    ),
+    suite("SimpleSubtype")(
+      test("read success") {
+        val value  = DynamoValue.fromNumber(123)
+        val result = DynamoFormat[SimpleSubtype].read(value)
+        assertTrue(result == Right(SimpleSubtype(123)))
+      },
+      test("read failure") {
+        val value  = DynamoValue.fromString("not a number")
+        val result = DynamoFormat[SimpleSubtype].read(value)
+        assertTrue(result.left.map(describe) == Left("not of type: 'N' was 'DynString(not a number)'"))
       }
     )
   )
