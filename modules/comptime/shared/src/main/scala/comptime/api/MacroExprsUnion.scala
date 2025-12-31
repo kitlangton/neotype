0750825d69fed127124139942fa2a9d201c77c74
Kit Langton <kit.langton@gmail.com>
2025-12-30 19:27:13 -0500

    style: format MacroExprsUnion.scala

--- modules/comptime/shared/src/main/scala/comptime/api/MacroExprsUnion.scala
+++ modules/comptime/shared/src/main/scala/comptime/api/MacroExprsUnion.scala
@@ -2,7 +2,9 @@ package comptime
 
 import scala.quoted.*
 
-/** Handles lifting union types (A | B) by checking which branch the runtime value belongs to. */
+/** Handles lifting union types (A | B) by checking which branch the runtime
+  * value belongs to.
+  */
 private[comptime] object MacroExprsUnion:
   def summonExprOpt[A: Type](using Quotes): Option[ToExpr[A]] =
     import quotes.reflect.*
@@ -18,24 +20,26 @@ private[comptime] object MacroExprsUnion:
         // Need at least one branch to work
         (leftOpt, rightOpt) match
           case (Some(teL), Some(teR)) =>
-            Some(new ToExpr[A]:
-              def apply(value: A)(using Quotes): Expr[A] =
-                // Try left branch first, fall back to right
-                try
-                  teL.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
-                catch
-                  case _: ClassCastException | _: MatchError =>
-                    teR.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
+            Some(
+              new ToExpr[A]:
+                def apply(value: A)(using Quotes): Expr[A] =
+                  // Try left branch first, fall back to right
+                  try teL.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
+                  catch
+                    case _: ClassCastException | _: MatchError =>
+                      teR.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
             )
           case (Some(te), None) =>
-            Some(new ToExpr[A]:
-              def apply(value: A)(using Quotes): Expr[A] =
-                te.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
+            Some(
+              new ToExpr[A]:
+                def apply(value: A)(using Quotes): Expr[A] =
+                  te.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
             )
           case (None, Some(te)) =>
-            Some(new ToExpr[A]:
-              def apply(value: A)(using Quotes): Expr[A] =
-                te.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
+            Some(
+              new ToExpr[A]:
+                def apply(value: A)(using Quotes): Expr[A] =
+                  te.asInstanceOf[ToExpr[Any]](value)(using summon[Quotes]).asExprOf[A]
             )
           case (None, None) => None
 
